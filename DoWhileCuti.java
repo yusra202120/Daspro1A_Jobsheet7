64b21167b066c514921e87bec557144880bd202d
Yusra Yusuf <yusrayusuf811@gmail.com>
2023-10-10 21:02:14 +0700

    Percobaan 3 no 3

---
+++ DoWhileCuti.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class DoWhileCuti {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+    do {
+        System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+        konfirmasi = sc.next();
+
+        if (konfirmasi.equalsIgnoreCase("y")) {
+            System.out.print("Jumlah hari: ");
+            jumlahHari = sc.nextInt();
+
+            if (jumlahHari <= jatahCuti) {
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti: " + jatahCuti);     
+            } else {
+                System.out.println("Sisa jatah cuti anda tidak mencukupi");
+    
+            }
+        }else if (konfirmasi.equalsIgnoreCase("t")) {
+            break;
+        }
+    } while (jatahCuti > 0);
+    }
+}
